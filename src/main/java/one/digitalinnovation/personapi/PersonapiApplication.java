25c128890065bc5437dc354a2d1648e757216194
carlos738 <carlosasl135@hotmail.com>
2022-01-26 20:15:13 -0300

    ApiRest

---
+++ src/main/java/one/digitalinnovation/personapi/PersonapiApplication.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
+@RequestMapping("/api/v1/people")
+
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}
